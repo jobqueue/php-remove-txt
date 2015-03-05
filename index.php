e20c51e86228eae154d469067d5b1d674885a074
jobqueue <tt.jobseekerGmail.com>
2015-03-05 13:51:11 -0500

    Update commented out diatomite match

--- index.php
+++ index.php
@@ -3,10 +3,9 @@ include 'vendor/autoload.php';
 
 $url_pdf = "https://ujsportal.pacourts.us/DocketSheets/MDJReport.ashx?docketNumber=MJ-19311-CR-0000113-2015";
 
-
 $parser = new \Smalot\PdfParser\Parser();
 $pdf    = $parser->parseFile($url_pdf);
 $text = $pdf->getText();
-//echo preg_replace("/Printed: \d+\/\d+\/\d{4}/", " ", (string)$text);
+//echo preg_replace("/Printed: \d+\/\d+\/\d{4} \d+:\d+ [aApP][mM]/", " ", $input_lines);
 echo preg_replace("/Printed:.* [aApP][mM]/", " ", (string)$text);
-?> 
+?>
