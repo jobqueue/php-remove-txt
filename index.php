cc3b756fbc1abc84fc25647b7709a96369ec40c0
jobqueue <tt.jobseekerGmail.com>
2015-03-05 14:15:52 -0500

    add more regex examples in comment

--- index.php
+++ index.php
@@ -6,6 +6,18 @@ $url_pdf = "https://ujsportal.pacourts.us/DocketSheets/MDJReport.ashx?docketNumb
 $parser = new \Smalot\PdfParser\Parser();
 $pdf    = $parser->parseFile($url_pdf);
 $text = $pdf->getText();
+//This attempts to match the datetime format
 //echo preg_replace("/Printed: \d+\/\d+\/\d{4} \d+:\d+ [aApP][mM]/", " ", $input_lines);
-echo preg_replace("/Printed:.* [aApP][mM]/", " ", (string)$text);
+//You can also borrow Gordo's regex for this one https://www.codersclan.net/ticket/896
+
+//echo preg_replace("/(Printed:)\s(\d+\/\d+\/\d{4})\s(\d+:\d+\s[aApP][mM])/", " ", (string)$text);
+//This tries to match anything from Printed: to Recent, not including Recent
+//echo preg_replace("/(Printed:)\s(\d+\/\d+\/\d{4})\s(\d+:\d+\s[aApP][mM])\s(?=Recent)/", "", (string)$text);
+
+//You could try your own regex for php here: http://www.phpliveregex.com/p/ahX
+
+
+//This tries to replace anything from Printed to AM/PM, including Printed and AM/PM
+//echo preg_replace("/Printed:.* [aApP][mM]/", " ", (string)$text);
+
 ?>
